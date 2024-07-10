a318d61c86bc6f88a2bc8682a045e0130e405e0a
victoriarosebender <victoria.rose.bender@gmail.com>
2024-07-10 19:10:02 +0000

    Homework 6 Tests

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,28 @@
 #
+
+import unittest
+from src.homework.i_dictionaries_and_sets import get_p_distance
+from src.homework.i_dictionaries_and_sets import get_p_distance_matrix 
+
+
+class Test_Config(unittest.TestCase):
+
+ 
+
+    def test_get_hamming_distance(self):
+        self.assertEqual(get_p_distance(['T','T','T','C','C','A','T','T','T','A'],['G','A','T','T','C','A','T','T','T','C']), 4)
+    
+     
+
+    def test_get_dna_complement(self):
+        self.assertEqual(get_p_distance_matrix ([
+ ['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']
+]), [
+ [0.0, 0.4, 0.1, 0.1],
+ [0.4, 0.0, 0.4, 0.3],
+ [0.1, 0.4, 0.0, 0.2],
+[0.1, 0.3, 0.2, 0.0]
+])
