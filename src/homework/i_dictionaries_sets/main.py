5d1ef48e7f46ee09a9eeff3c0164d3d6e69e6a95
victoriarosebender <victoria.rose.bender@gmail.com>
2024-07-10 20:23:00 +0000

    HW 6 Save

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -11,4 +11,25 @@ def get_p_distance(dna1,dna2):
 
     return count 
 
+
+
+
 def get_p_distance_matrix(matrix1):
+    rows = len(matrix1)
+    columns = len(matrix1)
+    master_matrix = []
+    ##row1.append(get_p_distance(list1,list2) / len(list1))
+
+    for i in range(1,rows):
+        row_list = []
+        for j in range(1,columns):
+            new_val = get_p_distance(matrix1[(i+1)]*matrix1[(j+1)]) / rows
+            row_list.append(new_val)
+        master_matrix.append(row_list)  
+    print(master_matrix)
+
+    
+get_p_distance_matrix([['T','T','T','C','C','A','T','T','T','A'],
+ ['G','A','T','T','C','A','T','T','T','C'],
+ ['T','T','T','C','C','A','T','T','T','T'],
+ ['G','T','T','C','C','A','T','T','T','A']])
