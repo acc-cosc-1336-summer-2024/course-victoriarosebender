ee74912b61be8887f618c431c70fa9a8077e8483
victoriarosebender <victoria.rose.bender@gmail.com>
2024-07-18 21:24:29 +0000

    hw 7 save pt 2

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,18 @@
 #
+
+import unittest
+from src.homework.j_classes.class_a import Die
+
+
+
+class Test_Config(unittest.TestCase):
+
+ 
+
+    def test_get_rolled_value(self):
+        die = Die()
+        die.roll()
+        
+        self.assertIn( die.get_roll_value(), range(1,7))
+    
+     
